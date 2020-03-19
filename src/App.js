226ecb1ee4664d4fa20ae79ac32cb9d52425e21e
Younes <younes-@hotmail.fr>
2020-03-19 15:46:26 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {HashRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import NavBar from './components/layout/NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+
+import backgroundImage from './pattern.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container">
-        <Dashboard />
+    <Router>
+      <div className="App" style={{background: `url(${backgroundImage})`}}>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
-import styled from 'styled-components' 
+import styled from 'styled-components'
+import spinner from './simple_pokeball.gif'
+import { Link } from 'react-router-dom'
+
 
 const Sprite = styled.img`
 width: 5em;
 height: 5em;
-`
+display: none;
+`;
+
+const Card = styled.div`
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+&:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)
+}
+-moz-user-select: none;
+-website-user-select: none;
+user-select: none;
+-o-user-select: none;
+`;
 
 
 export default class PokemonCard extends Component {
@@ -13,6 +29,8 @@ export default class PokemonCard extends Component {
         name:'',
         imageUrl: '',
         pokemonIndex: '',
+        imageLoading: true,
+        toManyRequest: false,
     };
     
     componentDidMount(){
@@ -27,23 +45,36 @@ export default class PokemonCard extends Component {
         })
     }
 
-
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <Link to={`pokemon/${this.state.pokemonIndex}`}>
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                    {this.state.imageLoading ? (
+                        <img src={spinner} style={{width: '5em', height: '5em'}} className="card-img-top rounded mx-auto d-block mt-2"></img>
+                        ): null}
                     <Sprite className="card-img-top rounded mx-auto mt-2"
-                    src={this.state.imageUrl}
-                    >
-                    </Sprite>
+                        onLoad={() => this.setState({imageLoading: false})}
+                        onError={() => this.setState({toManyRequest: true})}
+                        src={this.state.imageUrl}
+                        style={
+                            this.state.toManyRequest ? { display: "none" } :
+                            this.state.imageLoading ? null : { display: "block" }
+                        }
+                        />
+                    {this.state.toManyRequest ? (
+                        <h6 className="badge badge-danger mt-2">To Many Request</h6>
+                        ) : null}
+
                     <div className="card-body mx-auto">
                         <h6 className="card-title">
                             {this.state.name} 
                             {/* //TODO   I18N Majuscule première lettre */}
                         </h6>
                     </div>
-                </div>
+                </Card>
+                </Link>
             </div>
         )
     }
