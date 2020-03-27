ab3939c6fea59d9dff8b2e3c31d0cb25b1931316
Simon <simon.lebel17@gmail.com>
2020-03-27 14:21:13 +0100

    design

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
-import { bindActionCreators } from 'redux';
-import alltheActions from '../actions';
-import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React, {useEffect} from 'react'
+import {bindActionCreators} from "redux";
+import alltheActions from "../actions";
+import {connect} from "react-redux";
+import {useHistory} from "react-router-dom";
+
+import styled from 'styled-components'
 // import ReactPaginate from 'react-paginate'
 
 const ProfileScreen = props => {
@@ -16,7 +18,7 @@ const ProfileScreen = props => {
     }
   }, []);
   return (
-    <div>
+    <Container>
       <h1>ProfileScreen</h1>
       <dl>
         <dd>Email</dd>
@@ -24,18 +26,33 @@ const ProfileScreen = props => {
         <dd>Username</dd>
         <dt>{props.userState.user.username}</dt>
       </dl>
-    </div>
+    </Container>
   );
 };
 
-const mapDispatchToProps = () => dispatch => ({
-  actions: {
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  height: ${props => props.theme.height};
+  background-image: url(${props => props.theme.backgroundimage});
+  background-position: ${props => props.theme.backgroundposition};
+  background-repeat: ${props => props.theme.backgroundrepeat};
+  background-size: ${props => props.theme.backgroundsize};
+`;
+
+const mapDispatchToProps = () => dispatch =>({
+  actions:{
     userActions: bindActionCreators(alltheActions.userActions, dispatch)
   }
 });
 
 const mapStateToProps = state => ({
-  userState: state.userActions
+  userState: state.userActions,
+  themeState: state.theme
 });
 
 ProfileScreen.propTypes = {};
